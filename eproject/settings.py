384f2ebe9394be32c14cd81d2b273767bc7346b8
Eslam Ahmed <main1572000@gmail.com>
2026-07-28 14:05:58 +0300

    add email for sending message

--- eproject/settings.py
+++ eproject/settings.py
@@ -75,8 +75,12 @@ STORAGES = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'webmaster@localhost'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'eslamthetiger928@gmail.com'
+EMAIL_HOST_PASSWORD = 'rijwojzpthcf gkwh' # كلمة مرور التطبيقات من جوجل
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
